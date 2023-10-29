86ef80696571cfa310d30a31ba9fb9012714e2c9
Abir835 <abirhasan.raj.bd@gmail.com>
2023-10-29 15:59:57 +0600

    how to use redux

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Counter from './component/Counter';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>Welcome to React Redux</h1>
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
